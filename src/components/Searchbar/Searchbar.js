008a862256aeca2d8a02607f3e43c55de6ad8012
Placebo98 <oleg.tsaryk98@gmail.com>
2023-09-08 00:53:30 +0300

    add searchBar but withaut conection to MoviePage

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,27 @@
+import { BsFillSearchHeartFill } from 'react-icons/bs';
+
+import {
+  SearchBarWrapper,
+  SearchBarForm,
+  SearchBarButton,
+  SearchBarInput,
+} from './Searchbar.styled.js';
+
+export const SearchBar = ({ onSubmit }) => {
+  return (
+    <SearchBarWrapper>
+      <SearchBarForm
+        onSubmit={event => {
+          event.preventDefault();
+          onSubmit(event.target.elements.query.value.toLowerCase().trim());
+          event.target.reset();
+        }}
+      >
+        <SearchBarButton type="submit">
+          <BsFillSearchHeartFill size={25} color="black" />
+        </SearchBarButton>
+        <SearchBarInput name="query" type="text" placeholder="dog" />
+      </SearchBarForm>
+    </SearchBarWrapper>
+  );
+};
